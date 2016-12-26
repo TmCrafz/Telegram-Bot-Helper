0c2c9e7b3abe237ce7d700ad4810f2721f1c00d4
Tim <sterzeltim@gmail.com>
2016-12-27 00:51:40 +0100

    Implement Message sending

--- main.cpp
+++ main.cpp
@@ -1,10 +1,22 @@
 #include "include/CurlHelper.hpp"
+#include "include/TgBotHelper.hpp"
 #include <iostream>
 #include <string>
 
 int main(int argc, char *argv[])
 {
+	if (argc < 3) 
+	{
+		std::cout << "Please specify token and chatId" << std::endl;
+		return 0;
+	}
+	std::string token{ argv[1] };
+	std::string chatId{ argv[2] };
+	tgb::TgBotHelper tgBot(token);	
+	tgBot.sendMessage(chatId, "Hello Im a Bot =)");
+	std::cout << "Message send" << std::endl;
+
 	//std::cout << CurlHelper::simplePost("", "") << std::endl;
-	std::cout << "After simplePost" << std::endl;
+	//std::cout << "After simplePost" << std::endl;
 	return 0;
 }
--- src/TgBotHelper.cpp
+++ src/TgBotHelper.cpp
@@ -1,4 +1,5 @@
 #include "../include/TgBotHelper.hpp"
+#include "../include/CurlHelper.hpp"
 
 tgb::TgBotHelper::TgBotHelper(std::string token)
 : m_token{ token } 
@@ -8,5 +9,6 @@ tgb::TgBotHelper::TgBotHelper(std::string token)
 
 void tgb::TgBotHelper::sendMessage(std::string chatId, std::string message)
 {
-	
+	CurlHelper::simplePost("https://api.telegram.org/bot" + m_token + "/sendMessage", 
+			"chat_id=" + chatId + "&text=" + message);
 }
