d7fb8a9437bd3f8d7d5cdd629c75f236e0f6da57
Tim <sterzeltim@gmail.com>
2016-12-28 19:27:05 +0100

    Create files for message class

--- main.cpp
+++ main.cpp
@@ -16,15 +16,15 @@ int main(int argc, char *argv[])
 	std::string token{ argv[1] };
 	std::string chatId{ argv[2] };
 	tgb::TgBotHelper tgBot(token);	
-	bool success{ tgBot.sendMessage(chatId, "Hello Im a Bot =)") };
-	if (success)
-	{
-		std::cout << "Message send" << std::endl;
-	}
-	else
-	{
-		std::cout << "Message send failed" << std::endl;
-	}
+	//bool success{ tgBot.sendMessage(chatId, "Hello Im a Bot =)") };
+	//if (success)
+	//{
+		//std::cout << "Message send" << std::endl;
+	//}
+	//else
+	//{
+		//std::cout << "Message send failed" << std::endl;
+	//}
 	while(true)
 	{
 		std::cout << "New Updates: \n";
